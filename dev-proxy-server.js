e143c59bb368884ff7e0b691a5d20ed20c7c7711
Jan Illetschko <jan.illetschko@3kraft.com>
2019-01-08 12:11:49 +0100

    restructures config

--- dev-proxy-server.js
+++ dev-proxy-server.js
@@ -5,8 +5,16 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-const config = require('config');
 const createCertificate = require('./lib/createCertificate');
+
+process.env["NODE_CONFIG_DIR"] = process.env["NODE_CONFIG_DIR"] || __dirname + "/config";
+console.debug('using config dir:', process.env["NODE_CONFIG_DIR"]);
+
+process.env["NODE_ENV"] = process.env["NODE_ENV"] || 'development';
+console.debug('node env:', process.env["NODE_ENV"]);
+console.debug('node app instance:', process.env["NODE_APP_INSTANCE"] || '');
+
+const config = require('config');
 
 const httpsOptions = function () {
     const certPath = path.join(__dirname, './ssl/server.pem');
@@ -31,7 +39,7 @@ const httpsOptions = function () {
 console.debug('dev-proxy-server: starting using config:', JSON.stringify(config, null, 2));
 const app = express();
 
-const routes = config.util.toObject(config.get('proxy'));
+const routes = config.util.toObject(config['proxy'] || {});
 Object.getOwnPropertyNames(routes).forEach(path => {
     'use strict';
     const options = config.util.toObject(config.get(`proxy.${path}`));
@@ -39,7 +47,7 @@ Object.getOwnPropertyNames(routes).forEach(path => {
     app.use(proxy(path, options));
 });
 
-const port = config.get('port');
+const port = config['port'] || 3000;
 if (config.get('https')) {
     https.createServer(httpsOptions(), app).listen(port);
     console.info(`dev-proxy-server: listening for https on port: ${port}`);
