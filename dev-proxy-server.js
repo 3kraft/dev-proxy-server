03b3901ab56b6b2b664d8291c5a1ecb354c08304
Jan Illetschko <jan.illetschko@3kraft.com>
2024-02-14 12:01:08 +0100

    feat: console output via debug

--- dev-proxy-server.js
+++ dev-proxy-server.js
@@ -1,59 +1,67 @@
 #!/usr/bin/env node
 
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
-const express = require('express');
-const proxy = require('http-proxy-middleware');
-const createCertificate = require('./lib/createCertificate');
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const proxy = require("http-proxy-middleware");
+const createCertificate = require("./lib/createCertificate");
+const debug = require("debug");
 
-process.env["NODE_CONFIG_DIR"] = process.env["NODE_CONFIG_DIR"] || __dirname + "/config";
-console.debug('using config dir:', process.env["NODE_CONFIG_DIR"]);
+const log = debug("dev-proxy-server:main");
 
-process.env["NODE_ENV"] = process.env["NODE_ENV"] || 'development';
-console.debug('node env:', process.env["NODE_ENV"]);
-console.debug('node app instance:', process.env["NODE_APP_INSTANCE"] || '');
+process.env["NODE_CONFIG_DIR"] =
+  process.env["NODE_CONFIG_DIR"] || __dirname + "/config";
+log("using config dir:", process.env["NODE_CONFIG_DIR"]);
 
-const config = require('config');
+process.env["NODE_ENV"] = process.env["NODE_ENV"] || "development";
+log("node env:", process.env["NODE_ENV"]);
+log("node app instance:", process.env["NODE_APP_INSTANCE"] || "");
+
+const config = require("config");
 
 const httpsOptions = function () {
-    const certPath = path.join(__dirname, './ssl/server.pem');
+  const certPath = path.join(__dirname, "./ssl/server.pem");
 
-    if (!fs.existsSync(certPath)) {
-        const attrs = [
-            {name: 'commonName', value: 'localhost'}
-        ];
+  if (!fs.existsSync(certPath)) {
+    const attrs = [{ name: "commonName", value: "localhost" }];
 
-        const pems = createCertificate(attrs);
-        fs.writeFileSync(certPath, pems.private + pems.cert, {encoding: 'utf-8'});
-    }
+    const pems = createCertificate(attrs);
+    fs.writeFileSync(certPath, pems.private + pems.cert, { encoding: "utf-8" });
+  }
 
-    const cert = fs.readFileSync(certPath);
+  const cert = fs.readFileSync(certPath);
 
-    return {
-        key: cert,
-        cert: cert
-    };
+  return {
+    key: cert,
+    cert: cert,
+  };
 };
 
-console.debug('dev-proxy-server: starting using config:', JSON.stringify(config, null, 2));
+log(
+  "dev-proxy-server: starting using config:",
+  JSON.stringify(config, null, 2),
+);
 const app = express();
 
-const routes = config.util.toObject(config['proxy'] || {});
-Object.getOwnPropertyNames(routes).forEach(path => {
-    'use strict';
-    const options = config.util.toObject(config.get(`proxy.${path}`));
-    console.info('dev-proxy-server: adding proxy route:', path, '->', JSON.stringify(options, null, 2));
-    app.use(proxy(path, options));
+const routes = config.util.toObject(config["proxy"] || {});
+Object.getOwnPropertyNames(routes).forEach((path) => {
+  "use strict";
+  const options = config.util.toObject(config.get(`proxy.${path}`));
+  log(
+    "dev-proxy-server: adding proxy route:",
+    path,
+    "->",
+    JSON.stringify(options, null, 2),
+  );
+  app.use(proxy(path, options));
 });
 
-const port = config['port'] || 3000;
-if (config.get('https')) {
-    https.createServer(httpsOptions(), app).listen(port);
-    console.info(`dev-proxy-server: listening for https on port: ${port}`);
-
+const port = config["port"] || 3000;
+if (config.get("https")) {
+  https.createServer(httpsOptions(), app).listen(port);
+  log(`dev-proxy-server: listening for https on port: ${port}`);
 } else {
-    app.listen(port);
-    console.info(`dev-proxy-server: listening for http on port: ${port}`);
+  app.listen(port);
+  log(`dev-proxy-server: listening for http on port: ${port}`);
 }
-
